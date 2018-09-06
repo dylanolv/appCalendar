f92cae621a88eb863899b0fee3a5ec2cc81296f7
dylanolv <oliveira.dylann@gmail.com>
2018-09-06 16:05:01 +0200

    Modis FullCalendar

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -8,8 +8,14 @@ $(document).ready(function () {
         },
         defaultView: 'agendaDay',
         contentHeight: 'auto',
-        views: {
-        }
+        businessHours: {
+            // days of week. an array of zero-based day of week integers (0=Sunday)
+            dow: [1, 2, 3, 4], // Monday - Thursday
 
+            start: '09:00', // a start time (10am in this example)
+            end: '19:00', // an end time (6pm in this example)
+        },
+        views: {}
     });
+
 });
