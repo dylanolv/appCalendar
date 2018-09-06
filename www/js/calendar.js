5bd31836afe9624664cc3766a19fc94f06a357d7
dylanolv <oliveira.dylann@gmail.com>
2018-09-06 15:58:31 +0200

    Améliorations FullCalendar

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
 
     $('#calendar').fullCalendar({
-        defaultView: 'agendaWeek'
-    });
+        header: {
+            left: 'today',
+            center: 'prev,next,title', // buttons for switching between views
+            right: 'month,agendaWeek,agendaDay',
+        },
+        defaultView: 'agendaDay',
+        contentHeight: 'auto',
+        views: {
+        }
 
+    });
 });
