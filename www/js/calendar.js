524e42372094d44254984af40beb3e201d74a496
Farooq Asmaa <farooq.asmaa@gmail.com>
2018-09-06 16:29:29 +0200

    Add events on calendar

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -15,7 +15,49 @@ $(document).ready(function () {
             start: '09:00', // a start time (10am in this example)
             end: '19:00', // an end time (6pm in this example)
         },
-        views: {}
+        views: {},
+        events: function (start, end, timezone, callback) {
+            // let data = getCalendarData();
+            // {
+            //     $.ajax({
+            //         type: "GET",
+            //         contentType: "application/json; charset=utf-8",
+            //         url: "http://swiv.outofpluto.com:8082/api/lecture/lecture/",
+            //         dataType: 'json',
+            //         success: function (data) {
+            //             var events = [];
+            //             data.forEach(function(item, index) {
+            //                 events.push({
+            //                     title: item.title,
+            //                     start: item.date + "T" + item.start_time,
+            //                     end: item.date + "T" + item.end_time
+            //                 });
+            //             });
+            //             callback(events);
+            //         },
+            //         error: function (xhr, err) {
+            //             alert("ERROR! - readyState: " + xhr.readyState + "<br/>status: " + xhr.status + "<br/>responseText: " + xhr.responseText);
+            //         }
+            //     });
+
+            // }
+
+            let data = getCalendarData();
+            // console.log("data :", data);
+
+            let events = [];
+
+            data.forEach(function(item, index) {
+                events.push({
+                    title: item.title,
+                    start: item.date + "T" + item.start_time,
+                    end: item.date + "T" + item.end_time
+                });
+            });
+            callback(events);
+
+            // console.log("events :", events);
+        }
     });
 
 });
